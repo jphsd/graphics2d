71fc75f0b139c7df2562f150e2592321dd55a8d5
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2021-02-28 10:26:23 -0800

    Changes to example

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Still very much under development although the image functions are about as complete as I'm going to make them.
 
 The top level Path and Shape types are complete, what's missing are the useful PathProcessors in the rendering code.
+
+Package documentation [here](https://pkg.go.dev/github.com/jphsd/graphics2d)
--- image/cmd/morph.go
+++ image/cmd/morph.go
@@ -51,38 +51,37 @@ func main() {
 	saveImg(gray, "out")
 
 	// Perform a selection of operators
-	/*
-		suppt := All5x5
-		out := Open(gray, suppt)
-		saveImg(out, "out-open")
-		out = Close(gray, suppt)
-		saveImg(out, "out-close")
-		out = TopHat(gray, suppt)
-		saveImg(out, "out-top")
-		out = BotHat(gray, suppt)
-		saveImg(out, "out-bot")
-		out = gray
-		prev := &image.Gray{}
-		n := 0
-		for !Equals(prev, out) {
-			prev = out
-			out = Thin(out)
-			n++
-			if n%10 == 0 {
-				saveImg(out, fmt.Sprintf("out%d", n))
-				v, n := Variance(prev, out)
-				fmt.Printf("Variance %f over %d\n", v*float64(out.Bounds().Dx()*out.Bounds().Dy())/float64(n), n)
-			}
+	suppt := Z8
+	out := Open(gray, suppt)
+	saveImg(out, "out-open")
+	out = Close(gray, suppt)
+	saveImg(out, "out-close")
+	out = TopHat(gray, suppt)
+	saveImg(out, "out-top")
+	out = BotHat(gray, suppt)
+	saveImg(out, "out-bot")
+	out = gray
+	prev := &image.Gray{}
+	n := 0
+	for !Equals(prev, out) {
+		prev = out
+		out = Thin(out)
+		n++
+/*
+		if n%10 == 0 {
+			saveImg(out, fmt.Sprintf("out%d", n))
+			v, n := Variance(prev, out)
+			fmt.Printf("Variance %f over %d\n", v*float64(out.Bounds().Dx()*out.Bounds().Dy())/float64(n), n)
 		}
-		saveImg(out, fmt.Sprintf("out%d", n))
-	*/
-	suppt := Z4
-	out := LJSkeleton(gray, Z4, 32)
-	for i := 0; i < len(out); i++ {
-		saveImg(out[i], fmt.Sprintf("out%d", i))
+*/
 	}
-	out1 := LJReconstitute(out[:len(out)-1], suppt)
-	saveImg(out1, "out-recon")
+	saveImg(out, fmt.Sprintf("out-skel%d", n))
+
+	outs := LJSkeleton(gray, Z4, 32)
+	saveImg(outs[32], fmt.Sprintf("out-ljskel%d", 32))
+
+	out1 := LJReconstitute(outs[:32], suppt)
+	saveImg(out1, "out-ljrecon")
 }
 
 func saveImg(img image.Image, name string) {
