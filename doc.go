de5993e13209708cc5c115e2dd67ee28d23e23dd
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2021-03-27 22:08:30 -0700

    Updated package description

--- doc.go
+++ doc.go
@@ -11,12 +11,26 @@ The Shape type is a container for closed paths and represents something that can
 As Paths are added, if they're not already closed, they are forced closed.
 
 The PathProcessor interface is where the magic happens. Given a path, a function implementing this interface returns
-a collection of paths derived from it. This allows for stoking, dashing and a variety of other possibilities.
+a collection of paths derived from it. This allows for stroking, dashing and a variety of other possibilities.
 
 Shapes and Paths can be rendered with the render functions. Paths are forced closed when rendered.
 Convenience methods are provided for rendering with a single color or an image. The full render function
 allows a clip mask to be supplied and the draw.Op to be specified.
 
 The Aff3 type provides the ability to specify affine transforms on Paths and Shapes.
+
+Utility functions are provided to generate commmon forms of paths:
+  Point
+  Line
+  PolyLine
+  Curve
+  PolyCurve
+  Arc
+  ArcFromPoint
+  PolyArcFromPoint
+  Ellipse
+  EllipticalArc
+  EllipticalArcFromPoint
+  RegularPolygon
 */
 package graphics2d
