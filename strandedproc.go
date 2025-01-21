cc6971f618d2c1889286a793edafff344768ae76
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-01-21 11:01:51 -0800

    Added a path processor to create strands

---
+++ strandedproc.go
@@ -0,0 +1,33 @@
+package graphics2d
+
+// StrandedProc takes a path and converts it into a number of parallel paths.
+type StrandedProc struct {
+	Traces []*TraceProc
+}
+
+// NewStrandedProc returns a new stranded path processor.
+func NewStrandedProc(n int, w float64) *StrandedProc {
+	if n < 2 {
+		n = 2
+	}
+	if w < 0 {
+		w = -w
+	}
+	dw := w / float64(n-1)
+	w = -w / 2
+	traces := make([]*TraceProc, n)
+	for i := 0; i < n; i++ {
+		traces[i] = NewTraceProc(w)
+		w += dw
+	}
+	return &StrandedProc{traces}
+}
+
+// Process implements the PathProcessor interface.
+func (sp *StrandedProc) Process(p *Path) []*Path {
+	paths := []*Path{}
+	for _, tp := range sp.Traces {
+		paths = append(paths, tp.Process(p)...)
+	}
+	return paths
+}
