00a46b13b14d7000c629d0e6993e56a90b49cb46
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-07-09 15:37:15 -0700

    Updated image package doc

--- image/doc.go
+++ image/doc.go
@@ -1,52 +1,9 @@
 /*
-Package image contains functions that mostly operate on image.Gray.
-
-Logical Operations
-
-	And
-	Or
-	Xor
-	Not
-	Sub
-	Equal
-	Copy
-
-Morphological Operations
-
-	Dilate
-	Erode
-	Open
-	Close
-	TopHat
-	BotHat
-	HitOrMiss
-	Thin
-	Skeleton
-	LJSkeleton
-	LJReconstitute
-
-Remap Operations
-
-	ColorConvert
-	RemapGray
-	RemapRGBA
-
-Channel Operations
-
-	ExtractChannel
-	ReplaceChannel
-	SwitchChannels
-	CombineChannels
-
-Various metrics
-
-	Histogram
-	CDF
-	Variance
+Package image contains utility functions for reading, writing and converting images. 
 
 Image types:
 
-	Patch - replicates a patch of colors across the plane like Uniform does for a single color
-	Tile - replicates an image across the plane
+	[Patch] - replicates a patch of colors across the plane like Uniform does for a single color
+	[Tile] - replicates an image across the plane
 */
 package image
