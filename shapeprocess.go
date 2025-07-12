8d5eded7f41d350ffcb701a9f57c6e9519b4b205
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-07-12 14:28:38 -0700

    Added BucketProc shape processor

--- shapeprocess.go
+++ shapeprocess.go
@@ -1,5 +1,7 @@
 package graphics2d
 
+import "math/rand"
+
 // General purpose interface that takes a shape and turns it into
 // a slice of shapes.
 
@@ -15,9 +17,58 @@ type PathsProc struct{}
 func (pp *PathsProc) Process(s *Shape) []*Shape {
 	paths := s.Paths()
 	np := len(paths)
-	shapes := make([]*shapes, np)
+	shapes := make([]*Shape, np)
 	for i, path := range paths {
 		shapes[i] = NewShape(path)
+	}
+
+	return shapes
+}
+
+// BucketProc agregates paths into N shapes using the specificed style.
+type BucketProc struct {
+	N     int
+	Style BucketStyle
+}
+
+type BucketStyle int
+
+const (
+	Chunk BucketStyle = iota
+	RoundRobin
+	Random
+)
+
+// Process implements the ShapeProcessor interface.
+func (bp *BucketProc) Process(s *Shape) []*Shape {
+	shapes := make([]*Shape, bp.N)
+	paths := s.Paths()
+	np := len(paths)
+	b := 0
+	switch bp.Style {
+	case Chunk:
+		npb := np / bp.N
+		if npb < 1 {
+			npb = 1
+		}
+		for i, path := range paths {
+			shapes[b].AddPaths(path)
+			if i > 0 && i%npb == 0 {
+				b++
+			}
+		}
+	case RoundRobin:
+		for _, path := range paths {
+			shapes[b].AddPaths(path)
+			b++
+			if b == bp.N {
+				b = 0
+			}
+		}
+	case Random:
+		for _, path := range paths {
+			shapes[rand.Intn(bp.N)].AddPaths(path)
+		}
 	}
 
 	return shapes
