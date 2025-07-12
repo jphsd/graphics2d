b7026fc29b6f60617cf890bb008dafbd96a807db
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-07-12 10:09:09 -0700

    Added PathsProcess shape processor

--- shapeprocess.go
+++ shapeprocess.go
@@ -7,3 +7,18 @@ package graphics2d
 type ShapeProcessor interface {
 	Process(s *Shape) []*Shape
 }
+
+// PathsProc converts each path in a shape into its own shape.
+type PathsProc struct{}
+
+// Process implements the ShapeProcessor interface.
+func (pp *PathsProc) Process(s *Shape) []*Shape {
+	paths := s.Paths()
+	np := len(paths)
+	shapes := make([]*shapes, np)
+	for i, path := range paths {
+		shapes[i] = NewShape(path)
+	}
+
+	return shapes
+}
