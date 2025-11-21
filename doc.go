82a233b44e3fac35117d8fa8ac4fb1d2a673d0a2
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-11-21 08:35:21 -0800

    README update

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 Package graphics2d contains types and functions for 2D graphics rendering.
 
-An overview is presented in the above README (collapsed by default...).
+An comprehensive overview is presented in the above README, expand above to read it.
 */
 package graphics2d
