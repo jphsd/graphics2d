91ae51ecdc351e1158fef43c8ce4a8dd173d60ca
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-12-03 14:42:09 -0800

    Added ellipitcal arc example

--- eliparc_test.go
+++ eliparc_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jphsd/graphics2d/image"
 )
 
-// Use EllipticalArc to create a logo
+// An example using EllipticalArc to create a logo
 func ExampleEllipticalArc() {
 	width, height := 400, 400
 
@@ -26,14 +26,14 @@ func ExampleEllipticalArc() {
 	r2 := r * 0.85
 	path4 := g2d.EllipticalArcFromPoints2(p1, p2, r2, r2*rxy, 0, true, false, g2d.ArcOpen)
 
-	// Combine paths
+	// Combine paths into a shape
 	shape := g2d.NewShape(path1, path2, path3, path4)
 
-	// Rotate by 180 to get second half
+	// Rotate by 180 to get second half and add it
 	xfm := g2d.RotateAbout(g2d.Pi, 200, 200)
 	shape.AddShapes(shape.ProcessPaths(xfm))
 
-	// Rotate CCW by 60
+	// Rotate shape CCW by 60
 	xfm = g2d.RotateAbout(-g2d.Pi/6, 200, 200)
 	shape = shape.ProcessPaths(xfm)
 
